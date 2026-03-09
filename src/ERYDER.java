2b76467fe4ea23c06331a2698352bc523a57caa8
Fiona5114 <2458574511@qq.com>
2026-03-09 15:58:56 +0800

    ERYDER 2

---
+++ src/ERYDER.java
@@ -0,0 +1,62 @@
+public class ERYDER {
+    private String bikeID;
+    private int batteryLevel;
+    public boolean isAvailable;
+    public float kmDriven;
+
+    public ERYDER(String bikeID, int batteryLevel, boolean isAvailable, float kmDriven)
+    {
+        this.bikeID = bikeID;
+        this.batteryLevel = batteryLevel;
+        this.isAvailable = isAvailable;
+        this.kmDriven = kmDriven;
+    }
+
+    
+
+    public void ride()
+    {
+        if(isAvailable == true && batteryLevel >=20)
+        {
+           System.out.println("The bike is available.");
+        }
+        else
+        {
+            System.out.println("The bike is not available.");
+        }
+    }
+    
+    public ERYDER()
+    {
+    }
+
+    public void printBikeDetails()
+    {
+        System.out.println("The bike's ID is " + bikeID);
+        System.out.println("The battery level is " + batteryLevel);
+        if(isAvailable){System.out.println("The bike is available.");}
+        else {System.out.println("The bike is not available.");}
+        System.out.println("Its distance travelled in " + kmDriven + " km.");
+    }
+    public String getBikeID(){
+        return bikeID;
+    }
+
+    public void setBikeID(String bikeID){
+        this.bikeID = bikeID;
+    }
+
+    public int getBatteryLevel(){
+        return batteryLevel;
+    }
+
+    
+    public void getBatteryLevel(int batteryLevel){
+        if(batteryLevel>=0 && batteryLevel<=100){
+            this.batteryLevel = batteryLevel;
+        }
+        else {
+            System.out.println("The batteryLevel must be from 0 to 100");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ERYDER bike1 = new ERYDER();
+        bike1.printBikeDetails();
+        ERYDER bike2 = new ERYDER("1455zz",89,true,47.6f);
+        bike2.printBikeDetails();
+    }
+}
